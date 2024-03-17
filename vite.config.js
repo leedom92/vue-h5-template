d74819322a82c2ae3d6a945e88f9de290d7a759d
Leedom <jld1221@163.com>
2024-03-17 15:49:34 +0800

    feat: remove console & debugger when building

--- vite.config.js
+++ vite.config.js
@@ -9,26 +9,35 @@ import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
-    }),
-    Components({
-      resolvers: [VantResolver()],
-    }),
+export default defineConfig(({ command }) => (
+  {
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueDevTools(),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+      }),
+      Components({
+        resolvers: [VantResolver()],
+      }),
 
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      _a: fileURLToPath(new URL('./src/api', import.meta.url)),
-      _c: fileURLToPath(new URL('./src/components', import.meta.url)),
-      _i: fileURLToPath(new URL('./src/assets/images', import.meta.url)),
-      _u: fileURLToPath(new URL('./src/utils', import.meta.url)),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        _a: fileURLToPath(new URL('./src/api', import.meta.url)),
+        _c: fileURLToPath(new URL('./src/components', import.meta.url)),
+        _i: fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+        _u: fileURLToPath(new URL('./src/utils', import.meta.url)),
+      },
     },
-  },
-})
+    base: './',
+    server: {
+      open: true,
+    },
+    esbuild: {
+      drop: command === 'build' ? ['console', 'debugger'] : [],
+    },
+  }
+))
