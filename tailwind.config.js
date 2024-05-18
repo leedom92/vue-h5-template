383b8881d22923d6db7193414d0d699cadc8e292
Leedom <jld1221@163.com>
2024-05-18 12:47:36 +0800

    chore: tailwindcss preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ import colorPreset from '@leedomjs/tailwindcss-color-preset'
 
 export default {
   presets: [
-    colorPreset,
+    /**
+     * 因为该模版用的是vant-ui，所以预设会默认启用vant
+     *
+     * 下面配置可简写为：
+     * colorPreset()
+     *
+     * 如果你喜欢 element-ui 或者 naive-ui 的主题色，
+     * 你也可以启用它们：
+     * colorPreset({
+     *   element: true,
+     *   naive: true
+     * })
+     */
+    colorPreset({
+      vant: true,
+    }),
   ],
   content: ['./src/**/*.{vue,js}'],
   theme: {
