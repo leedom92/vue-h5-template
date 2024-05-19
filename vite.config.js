a467c335fa645a1188a5bf014b246cd1da42e24e
Leedom <jld1221@163.com>
2024-05-20 00:23:18 +0800

    revert: enable vite-plugin-vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// import VueDevTools from 'vite-plugin-vue-devtools'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
@@ -15,7 +14,7 @@ export default defineConfig(({ command }) => (
     plugins: [
       vue(),
       vueJsx(),
-      // VueDevTools(),
+      VueDevTools(),
       AutoImport({
         dts: true,
         imports: ['vue', 'vue-router', 'pinia'],
