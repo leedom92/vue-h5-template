a767830e1d6018707a86898c78b45fbf7fd9d276
Leedom <jld1221@163.com>
2024-07-31 01:01:11 +0800

    feat: add logo of deps in home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -19,32 +19,75 @@ onMounted(() => {
       <van-cell
         title="Vue 3"
         to="vue"
-      />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="vue" />
+        </template>
+      </van-cell>
+
       <van-cell
         title="Vite 5"
         url="https://vitejs.dev"
-      />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="vite" />
+        </template>
+      </van-cell>
+
       <van-cell
         title="Vue Router"
         url="https://router.vuejs.org"
-      />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="vue" />
+        </template>
+      </van-cell>
+
       <van-cell
         title="Pinia"
         url="https://pinia.vuejs.org"
-      />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="pinia" />
+        </template>
+      </van-cell>
       <van-cell
         title="Vant组件按需加载"
         url="https://vant-contrib.gitee.io/vant/v2/#/zh-CN/"
-      />
-      <van-cell title="Sass" />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="vant" />
+        </template>
+      </van-cell>
+
+      <van-cell title="Sass">
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="sass" />
+        </template>
+      </van-cell>
+
       <van-cell
         title="Tailwind CSS"
         url="https://tailwindcss.com"
-      />
+      >
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="tailwindcss" />
+        </template>
+      </van-cell>
+
+      <van-cell title="ESLint">
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="eslint" />
+        </template>
+      </van-cell>
+
+      <van-cell title="Stylelint">
+        <template #icon>
+          <svg-icon class="mr-2 size-6" name="stylelint" />
+        </template>
+      </van-cell>
       <van-cell title="NProgress.js进度条" />
       <van-cell title="Alias别名" />
-      <van-cell title="ESLint" />
-      <van-cell title="Stylelint" />
       <van-cell title="CSpell英文拼写问题检查" />
       <van-cell title="提交代码时eslint和stylelint检测，并自动修复" />
       <van-cell title="打包时移除console和debugger" />
