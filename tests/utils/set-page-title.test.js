5856d12a2c70e5e085b98330f68fb2b507cd663a
Leedom <jld1221@163.com>
2024-08-03 10:51:48 +0800

    test: improve unit test and add snapshot

--- tests/utils/set-page-title.test.js
+++ tests/utils/set-page-title.test.js
@@ -1,5 +1,9 @@
 import setPageTitle from '@/utils/set-page-title'
 
-it('setPageTitle', () => {
-  expect(setPageTitle('title')).toBe('title - Vue H5 Template')
+test('setPageTitle', () => {
+  expect(setPageTitle('title')).toBe('title - Vue H5 Template').toMatchSnapshot()
+})
+
+it('should be Vue H5 Template', () => {
+  expect(setPageTitle('')).toBe('Vue H5 Template').toMatchSnapshot()
 })
