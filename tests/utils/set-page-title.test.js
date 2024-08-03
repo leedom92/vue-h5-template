5d6b74b12c46db13b20eaf7aeceb8fd4953a7b85
Leedom <jld1221@163.com>
2024-08-03 16:16:01 +0800

    chore: improve test

--- tests/utils/set-page-title.test.js
+++ tests/utils/set-page-title.test.js
@@ -1,9 +1,11 @@
 import setPageTitle from '@/utils/set-page-title'
 
-test('setPageTitle', () => {
-  expect(setPageTitle('title')).toBe('title - Vue H5 Template').toMatchSnapshot()
-})
+describe('setPageTitle', () => {
+  it('has custom title', () => {
+    expect(setPageTitle('title')).toBe('title - Vue H5 Template').toMatchSnapshot()
+  })
 
-it('should be Vue H5 Template', () => {
-  expect(setPageTitle('')).toBe('Vue H5 Template').toMatchSnapshot()
+  it('should be Vue H5 Template', () => {
+    expect(setPageTitle('')).toBe('Vue H5 Template').toMatchSnapshot()
+  })
 })
