b397a87845c7573f02f322833a2422c6d9d312ce
Leedom <hi@leedom.me>
2025-04-13 21:05:24 +0800

    feat: bump eslit to v9 (#3)

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+
+// 导入 ESLint 推荐配置
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended, // 关键：添加 recommendedConfig
+  // 其他参数（如项目根目录）
+  baseDirectory: import.meta.dirname, // import.meta.dirname is available after Node.js v20.11.0
+})
+
+const eslintConfig = [
+  js.configs.recommended, // 直接使用推荐配置
+  ...compat.extends('@leedomjs/eslint-config-basic', '.eslintrc-auto-import.json'), // 兼容旧版 extends 语法
+]
+
+export default eslintConfig
